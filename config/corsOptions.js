927c49d16511cd9bb62c3b38cfd4ba8c1c339db9
Abubakarr Marah <telastudio2023@gmail.com>
2025-06-11 10:25:00 +0000

    CORS error resolved

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,8 +2,7 @@ import allowedOrigins from "./allowedOrigins.js"
 
 export const corsOptions = {
     origin: (origin, callback) => {
-        console.log(origin)
-        if(allowedOrigins.indexOf(origin) !== -1){
+        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
             callback(null, true);
         } else {
             callback(new Error("Not allowed by CORS"));
